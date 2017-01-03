ac7a902c6b314f593225b54c530d1032ea42281c
Julien <molasjulien@gmail.com>
2017-01-03 01:41:44 +0100

    All

---
+++ desktop/src/com/julien/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.julien.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.julien.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
