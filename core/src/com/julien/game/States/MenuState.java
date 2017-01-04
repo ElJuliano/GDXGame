622169f737078b19736a2f5a039652324bf8d364
Julien <molasjulien@gmail.com>
2017-01-04 01:02:50 +0100

    --Adding background images --Improving stateMenu

--- core/src/com/julien/game/States/MenuState.java
+++ core/src/com/julien/game/States/MenuState.java
@@ -1,6 +1,8 @@
 package com.julien.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.julien.game.MyGame;
 
 /**
  * Created by Julien on 04/01/2017.
@@ -8,9 +10,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuState extends State {
 
+    private Texture background;
+    private Texture playBtn;
 
     public MenuState(GameStateManager currentGsm) {
         super(currentGsm);
+        background = new Texture("background.jpg");
+        playBtn = new Texture("Button-Play-icon.png");
     }
 
     @Override
@@ -25,6 +31,9 @@ public class MenuState extends State {
 
     @Override
     protected void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0, MyGame.WIDTH, MyGame.HEIGHT);
+        sb.draw(playBtn, (MyGame.WIDTH)/2 -(playBtn.getWidth()/4), MyGame.HEIGHT/2 - playBtn.getHeight()/4, playBtn.getWidth()/2, playBtn.getHeight()/2);
+        sb.end();
     }
 }
