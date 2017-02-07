f6f7b630bd848fbb1c0e779382053125128ab8aa
Julien <molasjulien@gmail.com>
2017-02-07 01:19:37 +0100

    --Adding button background with pixmap object - Need to be redone in a better way

--- core/src/com/julien/game/sprites/flappy/GameOver.java
+++ core/src/com/julien/game/sprites/flappy/GameOver.java
@@ -1,6 +1,9 @@
 package com.julien.game.sprites.flappy;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +13,7 @@ import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.julien.game.states.GameStateManager;
@@ -25,8 +29,6 @@ public class GameOver {
     private static String PLAY_AGAIN = "Play Again";
 
     private BitmapFont font;
-    private float fontPositionX;
-    private float fontPositionY;
 
     private TextButton button;
     private TextButton.TextButtonStyle buttonStyle;
@@ -34,7 +36,7 @@ public class GameOver {
     private float buttonY;
 
     private GlyphLayout fontLayout;
-    private Button btn;
+    private Skin skin;
 
     private GameStateManager gsm;
 
@@ -47,14 +49,17 @@ public class GameOver {
             fontLayout = new GlyphLayout(font, GAME_OVER);
 
             buttonStyle = new TextButton.TextButtonStyle();
-            buttonStyle.fontColor = Color.BROWN;
             buttonStyle.font = new BitmapFont();
-            button = new TextButton(PLAY_AGAIN, buttonStyle);
+            buttonStyle.fontColor = Color.WHITE;
+            buttonStyle.overFontColor = Color.BLACK;
+
+            createBasicSkin();
+
+            button = new TextButton(PLAY_AGAIN, skin);
             button.setScale(3, 3);
             button.getLabel().setFontScale(4);
             button.setHeight(200);
             button.setWidth(300);
-            button.setColor(Color.BLUE);
             button.setBounds(buttonX, buttonY, 300, 200);
             button.setPosition(buttonX , buttonY);
 
@@ -74,6 +79,30 @@ public class GameOver {
                 gsm.set(new PlayState(gsm));
             }
         } );
+
+    }
+
+    private void createBasicSkin(){
+        //Create a font
+        BitmapFont font = new BitmapFont();
+        skin = new Skin();
+        skin.add("default", font);
+
+        //Create a texture
+        Pixmap pixmap = new Pixmap((int) Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.
+        pixmap.fill();
+        skin.add("background",new Texture(pixmap));
+
+        //Create a button style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+        textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+        textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
 
     }
 
